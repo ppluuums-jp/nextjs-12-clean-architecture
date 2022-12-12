f37f2e06e5fcc00e15ffbb5d359db01912dff3a2
ppluuums-jp <104049111+ppluuums-jp@users.noreply.github.com>
2022-12-12 22:54:54 +0900

    環境構築完了

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  <ChakraProvider>
+    return <Component {...pageProps} />
+  </ChakraProvider>;
 }
