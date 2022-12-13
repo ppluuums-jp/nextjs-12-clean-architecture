1b9a8889c9f65078e6cff5bdd45b5f5aafbc3e3f
Kodai Nakahara <nakahara.kodai@cloud-ace.jp>
2022-12-13 17:22:03 +0900

    Delete: init()

--- src/infrastructure/datastore/database/firestore/firestore-impl.ts
+++ src/infrastructure/datastore/database/firestore/firestore-impl.ts
@@ -12,15 +12,6 @@ export class FirestoreImpl implements Firestore {
     this.db = params.db;
   }
 
-  private init(): void {
-    firebaseAdmin.initializeApp({
-      credential: firebaseAdmin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
-      }),
-    });
-  }
   deleteUser(userId: string): Promise<void> {
     return Promise.resolve(undefined);
   }
