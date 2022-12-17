55e344dc357b0be137729e32ecf2229f9b48ab60
ppluuums-jp <104049111+ppluuums-jp@users.noreply.github.com>
2022-12-17 13:34:03 +0900

    titleを修正

--- src/presentation/components/pages/crud-page.tsx
+++ src/presentation/components/pages/crud-page.tsx
@@ -1,4 +1,4 @@
-import { Box, Spacer, Center, VStack } from "@chakra-ui/react";
+import { Box, Spacer, Center, VStack, Container } from "@chakra-ui/react";
 import React from "react";
 import { Title } from "../molecules/title";
 import { CrudButton } from "../organisms/crud-button";
@@ -6,13 +6,15 @@ import { CrudButton } from "../organisms/crud-button";
 export const CrudPage = (): JSX.Element => {
   return (
     <>
-      <Box h={"100vh"}>
-        <Title
-          props={{
-            title: "CRUD PAGE",
-          }}
-        ></Title>
-        <Center h={"90vh"}>
+      <Box>
+        <Container my={10}>
+          <Title
+            props={{
+              title: "CRUD PAGE",
+            }}
+          ></Title>
+        </Container>
+        <Center h={"70vh"}>
           <VStack direction={"column"}>
             <CrudButton />
           </VStack>
