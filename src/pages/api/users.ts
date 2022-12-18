8570265e2c68396e1cff8abbe02a316fa5cbbd3a
Kodai Nakahara <nakahara.kodai@cloud-ace.jp>
2022-12-19 04:23:41 +0900

    Update: 各APIの実装

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,7 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { container } from "../../di/inversify.config";
 import { TYPES } from "../../di/types";
+import { CreateUserUseCase } from "../../domain/usecases/create-user-usecase";
 import { ReadAllUsersUseCase } from "../../domain/usecases/read-all-users-usecase";
 
 export default async function handler(
@@ -9,7 +9,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   switch (req.method) {
-    case "GET":
+    case "GET": {
       const usecase = container.get<ReadAllUsersUseCase>(
         TYPES.ReadAllUsersUseCase
       );
@@ -17,7 +17,22 @@ export default async function handler(
       res.status(200);
       res.send(data);
       break;
+    }
+    case "POST": {
+      const body: CreateUserRequestBody = req.body;
+      const usecase = container.get<CreateUserUseCase>(TYPES.CreateUserUseCase);
+      await usecase.execute({ name: body.name, gender: body.gender });
+      res.status(200);
+      res.send(1);
+      break;
+    }
     default:
       res.status(400);
+      break;
   }
 }
+
+export type CreateUserRequestBody = {
+  name: string;
+  gender: "male" | "female" | "other";
+};
--- src/pages/api/users/[user-id].ts
+++ src/pages/api/users/[user-id].ts
@@ -1,16 +1,52 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { container } from "../../../di/inversify.config";
+import { TYPES } from "../../../di/types";
+import { DeleteUserUseCase } from "../../../domain/usecases/delete-user-usecase";
+import { ReadUserUseCase } from "../../../domain/usecases/read-user-usecase";
+import { UpdateUserUseCase } from "../../../domain/usecases/update-user-usecase";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const q = req.query;
-  // const router = useRouter();
-  // パスパラメータから値を取得
-  // const { userId } = router.query;
-  // const usecase = container.get<ReadUserUseCase>(TYPES.ReadUserUseCase);
-  // const users = await usecase.execute({ uuid: "dF3BnMurP0tRTio71Mxr" });
-  res.json(q);
-  res.status(200);
+  const userId = q["user-id"] as string;
+  switch (req.method) {
+    case "GET": {
+      const usecase = container.get<ReadUserUseCase>(TYPES.ReadUserUseCase);
+      const user = await usecase.execute({ uuid: userId });
+      res.status(200);
+      res.send(user);
+      break;
+    }
+    case "PUT": {
+      const body: UpdateUserRequestBody = req.body;
+      const usecase = container.get<UpdateUserUseCase>(TYPES.UpdateUserUseCase);
+      await usecase.execute({
+        uuid: userId,
+        name: body.name,
+        gender: body.gender,
+      });
+      res.status(200);
+      res.send(1);
+      break;
+    }
+    case "DELETE": {
+      const usecase = container.get<DeleteUserUseCase>(TYPES.DeleteUserUseCase);
+      await usecase.execute({
+        uuid: userId,
+      });
+      res.status(200);
+      res.send(1);
+      break;
+    }
+    default:
+      res.status(400);
+      break;
+  }
 }
+
+export type UpdateUserRequestBody = {
+  name: string;
+  gender: "male" | "female" | "other";
+};
