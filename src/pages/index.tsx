416c8b4142d39afbe1489c642169d253ef3e27db
ppluuums-jp <kodama.shinpei@cloud-ace.jp>
2022-12-20 14:13:01 +0900

    Added firebase setting files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,9 @@
-import { useToast } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import React from "react";
 import { RecoilRoot } from "recoil";
 import { CrudPage } from "../presentation/components/pages/crud-page";
 
 const Home: NextPage = () => {
-  const toast = useToast();
-  const insertUser = async () => {
-    // const res = await axios.get("/api/user");
-    toast({
-      title: "User created.",
-      description: "We've created your user for you.",
-      status: "success",
-      duration: 9000,
-      isClosable: true,
-    });
-    // console.log(res);
-  };
-
   return (
     <RecoilRoot>
       <CrudPage />
