5165cf48eed9df68ce30fafc69bb8baab460bb30
Kodai Nakahara <nakahara.kodai@cloud-ace.jp>
2022-12-22 15:58:57 +0900

    Update: Result対応
    #50

--- src/infrastructure/datastore/database/firestore/firestore-db.ts
+++ src/infrastructure/datastore/database/firestore/firestore-db.ts
@@ -1,14 +1,27 @@
+import { InternalError } from "../../../../core/error/internal-error";
+import { NetworkError } from "../../../../core/error/network-error";
+import { NotFoundError } from "../../../../core/error/not-found-error";
+import { Result } from "../../../../core/result";
 import { FSInsertUserParam } from "./model/insert-user-param";
 import { FSUpdateUserParam } from "./model/update-user-param";
 import { FSUser } from "./model/user";
 
 export interface FirestoreDB {
-  findAllUsers(): Promise<FSUser[]>;
-  findUserById(userId: string): Promise<FSUser>;
+  findAllUsers(): Promise<Result<FSUser[], InternalError | NetworkError>>;
 
-  insertUser(param: FSInsertUserParam): Promise<void>;
+  findUserById(
+    userId: string
+  ): Promise<Result<FSUser, NotFoundError | InternalError | NetworkError>>;
 
-  updateUser(param: FSUpdateUserParam): Promise<void>;
+  insertUser(
+    param: FSInsertUserParam
+  ): Promise<Result<boolean, InternalError | NetworkError>>;
 
-  deleteUser(userId: string): Promise<void>;
+  updateUser(
+    param: FSUpdateUserParam
+  ): Promise<Result<boolean, NotFoundError | InternalError | NetworkError>>;
+
+  deleteUser(
+    userId: string
+  ): Promise<Result<boolean, NotFoundError | InternalError | NetworkError>>;
 }
--- src/infrastructure/repositories/user-repository-impl.ts
+++ src/infrastructure/repositories/user-repository-impl.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "inversify";
+import "reflect-metadata";
 import { TYPES } from "../../di/types";
 import { User } from "../../domain/entities/user";
 import { UserRepository } from "../../domain/repositories/user-repository";
@@ -6,19 +7,24 @@ import { Gender } from "../../domain/values/gender";
 import type { FirestoreDB } from "../datastore/database/firestore/firestore-db";
 import { fsGenderConvertor } from "../datastore/database/firestore/model/gender";
 import { fsUserConverter } from "../datastore/database/firestore/model/user";
-import "reflect-metadata";
 
 @injectable()
 export class UserRepositoryImpl implements UserRepository {
   private readonly firestoreDB: FirestoreDB;
+
   constructor(@inject(TYPES.FirestoreDB) firestoreDB: FirestoreDB) {
     this.firestoreDB = firestoreDB;
   }
+
   async create(params: { name: string; gender: Gender }): Promise<void> {
-    await this.firestoreDB.insertUser({
+    const result = await this.firestoreDB.insertUser({
       name: params.name,
       gender: fsGenderConvertor.fromEntity(params.gender),
     });
+    if (result.isSuccess()) {
+    } else {
+      throw result.error;
+    }
   }
 
   async delete(param: { uuid: string }): Promise<void> {
@@ -26,13 +32,22 @@ export class UserRepositoryImpl implements UserRepository {
   }
 
   async findAll(): Promise<User[]> {
-    const users = await this.firestoreDB.findAllUsers();
-    return users.map(fsUserConverter.toEntity);
+    const result = await this.firestoreDB.findAllUsers();
+    if (result.isSuccess()) {
+      const users = result.value;
+      return users.map(fsUserConverter.toEntity);
+    } else {
+      throw result.error;
+    }
   }
 
   async findById(params: { uuid: string }): Promise<User> {
     const result = await this.firestoreDB.findUserById(params.uuid);
-    return fsUserConverter.toEntity(result);
+    if (result.isSuccess()) {
+      return fsUserConverter.toEntity(result.value);
+    } else {
+      throw result.error;
+    }
   }
 
   async update(params: {
@@ -40,10 +55,14 @@ export class UserRepositoryImpl implements UserRepository {
     name: string;
     gender: Gender;
   }): Promise<void> {
-    await this.firestoreDB.updateUser({
+    const result = await this.firestoreDB.updateUser({
       gender: fsGenderConvertor.fromEntity(params.gender),
       name: params.name,
       userId: params.uuid,
     });
+    if (result.isSuccess()) {
+    } else {
+      throw result.error;
+    }
   }
 }
