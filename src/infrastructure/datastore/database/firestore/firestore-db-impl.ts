7694463a505371ecd575564815a030e472ee5f83
Kodai Nakahara <nakahara.kodai@cloud-ace.jp>
2022-12-22 14:31:43 +0900

    Update firestore-db-impl.ts

--- src/infrastructure/datastore/database/firestore/firestore-db-impl.ts
+++ src/infrastructure/datastore/database/firestore/firestore-db-impl.ts
@@ -1,7 +1,9 @@
 import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
 import * as firestore from "firebase/firestore";
 import { injectable } from "inversify";
 import "reflect-metadata";
+import { NotFoundError } from "../../../../core/error/not-found-error";
 import { FirestoreDB } from "./firestore-db";
 import { FSInsertUserParam } from "./model/insert-user-param";
 import { FSUpdateUserParam } from "./model/update-user-param";
@@ -12,98 +14,76 @@ export class FirestoreDBImpl implements FirestoreDB {
   private readonly db: firestore.Firestore;
 
   constructor(params: { options: Object }) {
-    const app = initializeApp(params.options);
-    this.db = firestore.getFirestore(app);
+    if (!firebase.apps.length) {
+      initializeApp(params.options);
+    }
+    this.db = firestore.getFirestore();
   }
 
   async deleteUser(userId: string): Promise<void> {
-    const path = firestore.doc(this.db, FSCollectionPath.user, userId);
+    const collection = firestore.collection(this.db, FSCollectionPath.user);
+    const q = firestore.query(collection, firestore.where("id", "==", userId));
+    const qss = await firestore.getDocs(q);
+    if (qss.empty) {
+      throw new NotFoundError();
+    }
+    const path = firestore.doc(this.db, FSCollectionPath.user, qss.docs[0].id);
     await firestore.deleteDoc(path);
   }
 
   async findUserById(userId: string): Promise<FSUser> {
-    // const collection = await this.db.collection(FSCollectionPath.user);
-    // const doc = await collection.doc(userId);
-    // const ss = await doc.get();
-    // const data = ss.data();
-    // if (data == null) {
-    //   throw new NotFoundError();
-    // }
-    // const res: FSUser = {
-    //   createdAt: data["createdAt"],
-    //   gender: data["gender"],
-    //   id: data["id"],
-    //   name: data["name"],
-    //   updatedAt: data["updatedAt"],
-    // };
-    // return res;
-    return {
-      id: "",
-      name: "",
-      gender: 1,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
+    const collection = firestore.collection(this.db, FSCollectionPath.user);
+    const q = firestore.query(collection, firestore.where("id", "==", userId));
+    const qss = await firestore.getDocs(q);
+    if (qss.empty) {
+      throw new NotFoundError();
+    }
+    const doc = qss.docs[0];
+    return doc.data() as FSUser;
   }
 
   async insertUser(param: FSInsertUserParam): Promise<void> {
-    // const collection = await this.db.collection(FSCollectionPath.user);
-    // const doc = await collection.doc();
-    // const id = doc.id;
-    // const date = new Date();
-    // const user: FSUser = {
-    //   createdAt: date,
-    //   gender: param.gender,
-    //   id: id,
-    //   name: param.name,
-    //   updatedAt: date,
-    // };
-    // await doc.update(user);
+    const id = firestore.doc(this.db, FSCollectionPath.user).id;
+    const doc = firestore.doc(this.db, FSCollectionPath.user, id);
+    const date = new Date();
+    const user: FSUser = {
+      createdAt: date,
+      gender: param.gender,
+      id: id,
+      name: param.name,
+      updatedAt: date,
+    };
+    await firestore.setDoc(doc, user);
   }
 
   async updateUser(param: FSUpdateUserParam): Promise<void> {
-    // const oldUser = await this.findUserById(param.userId);
-    // const updatedAt = new Date();
-    // const user: FSUser = {
-    //   createdAt: oldUser.createdAt,
-    //   gender: param.gender ?? oldUser.gender,
-    //   id: param.userId,
-    //   name: param.name ?? oldUser.name,
-    //   updatedAt: updatedAt,
-    // };
-    // const collection = await this.db.collection(FSCollectionPath.user);
-    // const doc = await collection.doc(param.userId);
-    // await doc.update(user);
+    const collection = firestore.collection(this.db, FSCollectionPath.user);
+    const q = firestore.query(
+      collection,
+      firestore.where("id", "==", param.userId)
+    );
+    const qss = await firestore.getDocs(q);
+    if (qss.empty) {
+      throw new NotFoundError();
+    }
+    const oldUser = await this.findUserById(param.userId);
+    const updatedAt = new Date();
+    const user: FSUser = {
+      createdAt: oldUser.createdAt,
+      gender: param.gender ?? oldUser.gender,
+      id: param.userId,
+      name: param.name ?? oldUser.name,
+      updatedAt: updatedAt,
+    };
+    const doc = firestore.doc(this.db, FSCollectionPath.user, qss.docs[0].id);
+    await firestore.updateDoc(doc, user);
   }
 
   async findAllUsers(): Promise<FSUser[]> {
-    // const c = collection(this.db, FSCollectionPath.user);
-    // const ss = await collection.get();
-    // return ss.docs.map((v) => {
-    //   const data = v.data();
-    //   return {
-    //     createdAt: data["createdAt"],
-    //     gender: data["gender"],
-    //     id: data["id"],
-    //     name: data["name"],
-    //     updatedAt: data["updatedAt"],
-    //   };
-    // });
-
-    const q = firestore.query(
-      firestore.collection(this.db, FSCollectionPath.user)
-    );
+    const collection = firestore.collection(this.db, FSCollectionPath.user);
+    const q = firestore.query(collection);
     const qss = await firestore.getDocs(q);
-    return qss.docs.map((v) => {
-      const data = v.data();
-      return {
-        createdAt: data["createdAt"],
-        gender: data["gender"],
-        id: data["id"],
-        name: data["name"],
-        updatedAt: data["updatedAt"],
-      };
-    });
+    return qss.docs.map((v) => v.data() as FSUser);
   }
 }
 
