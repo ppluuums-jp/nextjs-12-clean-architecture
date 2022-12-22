6df56bf88f9da1fe0ea667aec970fc8fae946e06
Kodai Nakahara <nakahara.kodai@cloud-ace.jp>
2022-12-22 16:12:36 +0900

    Update: usecaseのResult対応
    #50

--- src/domain/usecases/update-user-usecase.ts
+++ src/domain/usecases/update-user-usecase.ts
@@ -1,8 +1,9 @@
+import { Result } from "../../core/result";
 import { Gender } from "../values/gender";
 import { UseCase } from "./usecase";
 
 export interface UpdateUserUseCase
-  extends UseCase<Promise<void>, UpdateUserUseCaseParam> {}
+  extends UseCase<Promise<Result<boolean, Error>>, UpdateUserUseCaseParam> {}
 
 export type UpdateUserUseCaseParam = {
   uuid: string;
