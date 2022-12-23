22120f0343d615d184655b838bf5e020b5eb7730
Kodai Nakahara <nakahara.kodai@cloud-ace.jp>
2022-12-23 15:39:51 +0900

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   verbose: true,
-  setupFilesAfterEnv: ["<rootDir>/testSetupFile.js"],
+  // setupFilesAfterEnv: ["<rootDir>/testSetupFile.js"],
+  // testMatch: ["**/?(*.)+(spec|test).+(ts|tsx|js)"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  // moduleNameMapper: {
+  //   "^@/(.*)$": "<rootDir>/src/$1", // 追加
+  // },
 };
